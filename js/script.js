4cc5f6811c0374a87ce0a5cc1ddecb7059a9eb89
abderah-web <abderrahim.lhaloui2003@gmail.com>
2026-02-19 15:28:09 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,27 @@ function unDo() {
     x.innerHTML = "Hover over a car below";
     x.style.backgroundImage="";
 }
+function setimages() {
+    currentimages = document.querySelectorAll("#imgs img")
+    for ( var i=0 ; i < currentimages.length ; i++ ) {
+    
+    currentimages[i].setAttribute("tabindex", "0")
+        
+    }
+
+}
+function focusimg(focus) {
+    
+const x = document.getElementById("image");
+    x.style.backgroundImage = `url('${focus.src}')`;
+    x.style.backgroundSize = "cover";
+    x.style.backgroundPosition = "center";
+    x.innerHTML = focus.alt;
+
+}
+
+function unfocus() {
+    const x = document.getElementById("image");
+    x.innerHTML = "Hover over a car below";
+    x.style.backgroundImage="";
+}
